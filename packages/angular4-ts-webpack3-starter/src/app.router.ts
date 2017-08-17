54c8544072e3fa026bc9f663b6851d7e5fe9023a
mrdulin <novaline.dulin@gmail.com>
2017-08-17 12:24:23 +0800

    找到aot编译成功，运行时报错的原因

--- packages/angular4-ts-webpack3-starter/src/app.router.ts
+++ packages/angular4-ts-webpack3-starter/src/app.router.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 
 import { CollectionComponent } from './collection';
 import { EditComponent } from './edit';
-import { loadListModule } from './list';
+
+import { loadListModule } from './+list';
 
 export const rootRouterConfig: Routes = [
   {
@@ -12,7 +13,12 @@ export const rootRouterConfig: Routes = [
   },
   {
     path: '',
-    loadChildren: loadListModule
+
+    // 注意：下面这种方式，aot编译成功，但是通过webpack打包时，angular-router-loader不能正确处理
+    // 报错：https://stackoverflow.com/questions/45713345/angular2-runtime-compiler-is-not-loaded
+    // https://github.com/brandonroberts/angular-router-loader/issues/85
+    // loadChildren: loadListModule
+    loadChildren: './+list/list.module#ListModule?chunkName=ListModule'
   },
   {
     path: '',
